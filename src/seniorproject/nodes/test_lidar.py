2f7eb7929f75a8db9687d9e98deed6910c80a356
kbienz <kalonbienz@gmail.com>
2025-02-12 07:04:12 +0900

    test_lidar.py properly finds forward distance now (instead of just point #50)

--- src/seniorproject/nodes/test_lidar.py
+++ src/seniorproject/nodes/test_lidar.py
@@ -12,20 +12,21 @@ class TestLidar:
         self.sub_lidar_data = rospy.Subscriber('/scan', LaserScan, self.cbFindForwardDistance, queue_size=1)
         # Changed publisher to publish float value instead of compressed image
         self.pub_forward_distance = rospy.Publisher('/forward_distance', Float32, queue_size=1)
-        print("Successfuly initialized LIDAR test node!")
+        print("1 Successfuly initialized LIDAR test node!")
 
     def cbFindForwardDistance(self, lidar_data):
         try:
-            # Access the 50th element safely and handle potential inf/nan values
-            forward_distance = lidar_data.ranges[50]
-            if not np.isinf(forward_distance) and not np.isnan(forward_distance):
-                print(f"Forward distance: {forward_distance}")
+            # Access the middle element safelyy and handle potential inf/nan values
+            num_points = int(6.28//lidar_data.angle_increment)
+            forward_distance = lidar_data.ranges[0]
+            if forward_distance>0 and not np.isinf(forward_distance) and not np.isnan(forward_distance):
+                print(f"Forward distance: {round(forward_distance, 3)} Meters | Points Collected: {num_points}")
                 # Publish the distance as a Float32 message
                 msg = Float32()
                 msg.data = forward_distance
                 self.pub_forward_distance.publish(msg)
         except IndexError:
-            rospy.logwarn("Index 50 is out of range for LiDAR data")
+            rospy.logwarn("Index error")
 
     def main(self):
         rospy.spin()
