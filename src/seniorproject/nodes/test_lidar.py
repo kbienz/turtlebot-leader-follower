14b3a99bafb8186459b573cba439057d5a8a719f
kbienz <kalonbienz@gmail.com>
2025-02-13 07:09:28 +0900

    Added LIDAR filtering

--- src/seniorproject/nodes/test_lidar.py
+++ src/seniorproject/nodes/test_lidar.py
@@ -9,24 +9,31 @@ from std_msgs.msg import Float32  # Added for publishing distance
 class TestLidar:
     def __init__(self):
         # Remove unnecessary parameters since we're only doing LiDAR processing
-        self.sub_lidar_data = rospy.Subscriber('/scan', LaserScan, self.cbFindForwardDistance, queue_size=1)
+        self.sub_lidar_data = rospy.Subscriber('/scan_filtered', LaserScan, self.cbFindForwardDistance, queue_size=1)
         # Changed publisher to publish float value instead of compressed image
         self.pub_forward_distance = rospy.Publisher('/forward_distance', Float32, queue_size=1)
-        print("1 Successfuly initialized LIDAR test node!")
+        print("Successfuly initialized LIDAR test node!")
+        self.previous_distance = 0
+        self.forward_distance = 0
+
 
     def cbFindForwardDistance(self, lidar_data):
-        try:
-            # Access the middle element safelyy and handle potential inf/nan values
-            num_points = int(6.28//lidar_data.angle_increment)
-            forward_distance = lidar_data.ranges[0]
-            if forward_distance>0 and not np.isinf(forward_distance) and not np.isnan(forward_distance):
-                print(f"Forward distance: {round(forward_distance, 3)} Meters | Points Collected: {num_points}")
-                # Publish the distance as a Float32 message
-                msg = Float32()
-                msg.data = forward_distance
-                self.pub_forward_distance.publish(msg)
-        except IndexError:
-            rospy.logwarn("Index error")
+            try:
+                num_points = len(lidar_data.ranges)
+                #mid_point = num_points//2
+                self.forward_distance = min(lidar_data.ranges[0],  lidar_data.ranges[-1], lidar_data.ranges[+1],
+                lidar_data.ranges[-2], lidar_data.ranges[+2])
+                self.forward_velocity = (self.forward_distance - self.previous_distance)
+                if self.forward_distance>0 and not np.isinf(self.forward_distance) and not np.isnan(self.forward_distance):
+                    print(f"Dist: {round(self.forward_distance, 2)} || Vel: {round(self.forward_velocity, 2)}")
+                    # Publish the distance as a Float32 message
+                    msg = Float32()
+                    msg.data = self.forward_distance
+                    self.pub_forward_distance.publish(msg)
+                    self.previous_distance = self.forward_distance
+                
+            except Exception as e:
+                rospy.logerr(f"Error in LiDAR callback: {e}")
 
     def main(self):
         rospy.spin()
